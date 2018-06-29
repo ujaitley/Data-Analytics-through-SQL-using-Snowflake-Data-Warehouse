506e3a584fe99449ecdfb4f7d55c9b271d61a2a9
Urvashi Jaitley <40471839+ujaitley@users.noreply.github.com>
2018-06-28 18:44:56 -0700

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,26 +3,11 @@ import pandas as pd
 df = pd.read_csv('movies_metadata.csv')
 df.info()
 
-'''OUTPUT Sample
-
-RangeIndex: 45466 entries, 0 to 45465
-Data columns (total 24 columns):
-adult                    45466 non-null object
-belongs_to_collection    4494 non-null object
-budget                   45466 non-null object
-genres                   45466 non-null object
-homepage                 7782 non-null object
-id                       45466 non-null object
-'''
-
-
-'''From the results we observe that there are many columns like budget whose datatype should be int/float, but its datatype is object,
-which needs to be change. One reason of mismatch datatype could be that there can be few non numeric values (string)
-present in numeric columns, due to which there datatype is listed as object. So, we need to check for non-numeric values 
+'''From the results we observe that budget column datatype should be int/float, but its datatype is object(budget45466 non-null object),
+which needs to be change. One reason of mismatch datatype could be that there can be few non numeric values (string)present in numeric columns, due to which there datatype is listed as object. So, we need to check for non-numeric values 
 in numeric columns.'''
 
 #Search for the rows having non numeric values in budget column, so that we can decide whether we can drop them or replace them with 0/ mean etc.
-
 print (df[pd.to_numeric(df['budget'], errors='coerce').isnull()])
 
 "'Output
@@ -35,11 +20,6 @@ print (df[pd.to_numeric(df['budget'], errors='coerce').isnull()])
 19730              0.065736  /ff9qCepilowshEtG2GYWwzt2bs4.jpg   
 29503              1.931659  /zV8bHuSL6WXoD6FWogP9j4x80bL.jpg   
 35587              2.185485  /zaSf5OG7V8X8gqFvly88zDdRm46.jpg   
-
-                                                  genres  \
-19730  [{'name': 'Carousel Productions', 'id': 11176}...   
-29503  [{'name': 'Aniplex', 'id': 2883}, {'name': 'Go...   
-35587  [{'name': 'Odyssey Media', 'id': 17161}, {'nam...   
 
                                                 homepage          id imdb_id  \
 19730  [{'iso_3166_1': 'CA', 'name': 'Canada'}, {'iso...  1997-08-20       0   
@@ -61,26 +41,21 @@ print (df[pd.to_numeric(df['budget'], errors='coerce').isnull()])
 29503      NaN    NaN   NaN          NaN        NaN  
 35587      NaN    NaN   NaN          NaN        NaN  
 
-[3 rows x 24 columns] '''
-
-'''From the above output we can see that there are 3 rows having non-numeric values in budget column. Due to which
-budget column was listed as object datatype. It will be wiser to drop these 3 rows  as these rows have many NaN values and 
+'''From the above output we can see that there are 3 rows having non-numeric values in budget column. Due to which budget column was listed as object datatype. It will be wiser to drop these 3 rows  as these rows have many NaN values and 
 many mismatch datatypes other than budget, like original_language (should have string but have int values)'''
 
 #Droping rows
 df = df[pd.to_numeric(df['budget'], errors='coerce').notnull()]
 
 #Output : new number of entries will be 45463 
-
-#If we don't want to drop whole row, beacuse it might have useful info in other columns then we can just drop values from that
-particular cell (like in this case 3 values only from budget column will be dropped instead of complete rows) while converting the column to numeric datatype
+'''If we don't want to drop whole row, beacuse it might have useful info in other columns then we can just drop values from that
+particular cell (like in this case 3 values only from budget column will be dropped instead of complete rows) while converting the column to numeric datatype'''
 
 df.budget = pd.to_numeric(df.budget, errors='coerce')
 
 #Output : In total,number of entries will be 45466, but in budget column it will be 45463
 
 #To check total number of null values in all columns
-
 df.isnull().sum() 
 
 '''Output Sample
